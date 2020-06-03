aa0d35c5963f54cd58ded3b4a5c1ac3a908e58bf
Glauber Fernandes <glaubersfernandes@gmail.com>
2020-06-03 15:37:04 -0300

    Segundo dia #nlw

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-    //JSON
-
-    response.json([
-        'Glauber',
-        'Gabriela',
-        'Gabrielly',
-        'Eduarda',
-        'João Guilherme',
-        'Jambalaya',
-        'Bianca'
-    ]);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
